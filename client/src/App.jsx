429da18417b0295790dd54eeb2785785f72a9cc3
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2025-04-01 23:21:16 -0400

    solve post req bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
   const handleReserveSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/reserve', {
+      const response = await fetch('/api/reserve', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
